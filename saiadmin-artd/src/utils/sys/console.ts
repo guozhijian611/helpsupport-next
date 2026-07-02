cfb44fee29931c68d21531078c55b63a826c60ae
allen <allen@openb8.org>
2026-07-02 14:54:41 +0800

    fix: 修正后台品牌名称为治疗支持

--- saiadmin-artd/src/utils/sys/console.ts
+++ saiadmin-artd/src/utils/sys/console.ts
@@ -1,6 +1,6 @@
 // ANSI 转义码生成网站  https://patorjk.com/software/taag/#p=display&f=Big&t=ABB%0A
 const asciiArt = `
-\x1b[32m欢迎使用 治疗之触 HelpSupport
+\x1b[32m欢迎使用 治疗支持 HelpSupport
 \x1b[0m
 \x1b[36mHelpSupport 后台管理系统
 \x1b[0m
